03e13c89f5881df32f35590ebca8a604128c7d41
Leonel Valencia <valenciaestudillo@gmail.com>
2022-11-25 17:56:17 -0600

    add links

--- admin/indexadmin.php
+++ admin/indexadmin.php
@@ -22,42 +22,70 @@
 <body>
     <header>
         <nav id="menu-h">
-            <img src="./assets/images/logo.png" alt="">
+            <img src="../assets/images/logo.png" alt="">
             <ul>
-                <li><a href="#">Home</a></li>
+                <li><a href="./indexadmin.php">Home</a></li>
                 <li><a href="#">Mis usuarios</a>
                     <ul class="submenu">
-                        <li><a href="#">Agregar nuevo</a></li>
-                        <li><a href="#">Usuarios</a></li>
+                        <li><a href="./agregarUsuarios2.html">Agregar nuevo</a></li>
+                        <li><a href="./UsuariosRegistrados.html">Usuarios</a></li>
                     </ul>
                 </li>
                 <li><a href="#">Dieta base</a>
                     <ul class="submenu">
-                        <li><a href="#">Crear dieta</a></li>
-                        <li><a href="#">Compartir</a></li>
-                        <li><a href="#">Ver mis dietas</a></li>
+                        <li><a href="./dietas/dietaManual.html">Crear dieta</a></li>
+                        <li><a href="./dietas/compartir.html">Compartir</a></li>
+                        <li><a href="./dietas/historialDietas.html">Ver mis dietas</a></li>
                     </ul>
                 </li>
                 <li><a href="#">Citas</a>
                     <ul class="submenu">
-                        <li><a href="#">Agregar cita</a></li>
+                        <li><a href="./agendarCitas.html">Agregar cita</a></li>
                         <li><a href="#">(1)Peticion de cita</a></li>
                     </ul>
                 </li>
                 <li><a href="#">Herramientas</a>
                     <ul class="submenu">
-                        <li><a href="#">Calculadora IMC</a></li>
+                        <li><a href="./calculadoraIMC.html">Calculadora IMC</a></li>
                     </ul>
                 </li>
                 <li><a href="#">Pagos</a></li>
-                <li><a href="#">Iniciar sesion</a></li>
-                <li><a href="cerrarsesion.php">Cerrar sesion</a></li>
+                <li><a href="#">Username</a>
+                    <ul class="submenu">
+                        <li><a href="./cerrarsesion.php">Cerrar sesion</a></li>
+                    </ul>
+                </li>
             </ul>
         </nav>
     </header>
     <main>
-        <h1>Index ADMIN</h1>
-        
+       <img src="./assets/images/principal.png" alt="frutas y versuras" id="imgPrincipal">
+        <h1 class="titulo">Hola bienvenido al sitio NUTRICION ES MAS</h1>
+        <section id="contactanos">
+            <div id="divUbicacion">
+                <img src="./assets/icons/home.svg" alt="ubicacion">
+                <h3>UBICACIÓN</h3>
+                <h4>Morelos, México</h4>
+            </div>
+            <div id="divTelefono">
+                <img src="./assets/icons/phone.svg" alt="ubicacion">
+                <h3>TELÉFONO</h3>
+                <h4><a href="https://l.instagram.com/?u=https%3A%2F%2Fwalink.co%2F9f767d&e=ATNkkNgb3IMtpTGmbzQBfXqaGSxHnWArHe104hS_jrwkIvZBET3ur-WMl8Ebus4D1UuirnxAFmJNLpaw3rL8jSo&s=1">(+52) 777 446 61 55</a></h4>
+            </div>
+            <div id="divEmail">
+                <img src="./assets/icons/envelope.svg" alt="ubicacion">
+                <h3>EMAIL</h3>
+                <h4>katya.os.nut@gmail.com</h4>
+            </div>
+            <div id="divSocial">
+                <img src="./assets/icons/hashtag.svg" alt="ubicacion">
+                <h3>SOCIAL MEDIA</h3>
+                <div id="socialMedia">
+                    <a href="https://www.facebook.com/profile.php?id=100087171421826"><img src="./assets/icons/facebook.svg" alt="facebook"></a>
+                    <a href="https://www.instagram.com/nutri.katya.osorio/"><img src="./assets/icons/instagram.svg" alt="instagra"></a>
+                </div>
+            </div>
+        </section>
     </main>
     <footer>
         <h5>Sitio diseñado por equipo 6</h5>
